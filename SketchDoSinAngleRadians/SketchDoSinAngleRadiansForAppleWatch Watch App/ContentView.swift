33c9bfaa3e54e696999fcba18c296cce98fb773e
Juan <jmoreno@novopayment.com>
2024-08-23 15:28:31 -0500

    Add project for Apple Watch

---
+++ SketchDoSinAngleRadians/SketchDoSinAngleRadiansForAppleWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SketchDoSinAngleRadiansForAppleWatch Watch App
+//
+//  Created by Juan Manuel Moreno on 23/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SketchDoSinAngleRadians/SketchDoSinAngleRadiansForAppleWatch Watch App/SketchDoSinAngleRadiansForAppleWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SketchDoSinAngleRadiansForAppleWatchApp.swift
+//  SketchDoSinAngleRadiansForAppleWatch Watch App
+//
+//  Created by Juan Manuel Moreno on 23/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SketchDoSinAngleRadiansForAppleWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
